499b0755f60ddc8ab96bdaabff8eb0f82287417b
jp4g <gilcrejp@miamioh.edu>
2019-03-03 12:55:19 -0600

    final2

--- truffle.js
+++ truffle.js
@@ -1,8 +1,3 @@
-require('dotenv').config();
-const HDWalletProvider = require('truffle-hdwallet-provider');
-
-
-
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -12,18 +7,9 @@ module.exports = {
       runs: 200
     }
   },
-  networks: {
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(process.env.MNEMONIC, process.env.INFURA_API)
-      },
-      gasPrice: 4000000000,
-      network_id: 3
-    },
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*" // match any network id
-    }
+  development: {
+    host: "127.0.0.1",
+    port: 8545,
+    network_id: "*" // match any network id
   }
 };
